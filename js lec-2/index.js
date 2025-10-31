585a44e66f1aad5962e2ac581b2ff05a9847c0e5
AbdulMoid-20 <abdulmoidsiddiqui2008@gmail.com>
2025-10-31 10:39:16 +0500

    first commit

---
+++ js lec-2/index.js
@@ -0,0 +1,27 @@
+// let name = "Abdul Moid";
+// let age = "18";
+// let city = "Hyderabad";
+
+// age = "20"
+// console.log(name +" " + age +" " + city);
+
+// var name = "Abdul Moid";
+// var age = "18";
+// var city = "Hyderabad";
+
+// var age = "20"
+// console.log(name +" " + age +" " + city);
+
+// const name = "Abdul Moid";
+// const age = "18";
+// const city = "Hyderabad";
+
+// const age = "20"
+// console.log(name +" " + age +" " + city);
+
+let name = prompt("What is your name?");
+if (name) {
+    console.log("Hello, " + name + "!");
+} else {
+    console.log("You didn't enter your name.");
+}
