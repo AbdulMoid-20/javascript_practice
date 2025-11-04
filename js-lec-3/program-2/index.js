d1d799e5101ac1cc5985ceea1be70b5deee72df2
AbdulMoid-20 <abdulmoidsiddiqui2008@gmail.com>
2025-11-04 18:27:13 +0500

    first commit

--- js-lec-3/program-2/index.js
+++ js-lec-3/program-2/index.js
@@ -1,9 +1,24 @@
-// let num1 = 10;
-// let num2 = num1;
-// num2 = 20 ;
-// console.log(num2);
-// console.log(num1);
+// BY Value
+
+let num1 = 10;
+let num2 = num1;
+
+num2 = 20;
+
+console.log(num2);
+console.log(num1);
 
 // by value it copies actual value
 
-let 
+
+// BY Reference
+
+let book = {name : 'No name'} ;
+let book2 = book ;
+
+book2.name = 'Abdul Moid';
+
+console.log(book2);
+console.log(book);
+
+// by reference it copies the reference of the value
