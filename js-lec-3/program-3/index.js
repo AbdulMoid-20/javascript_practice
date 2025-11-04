21e08c981ce2be55ed81228effd2ef41a5f6e5d3
AbdulMoid-20 <abdulmoidsiddiqui2008@gmail.com>
2025-11-04 18:39:51 +0500

    first commit

---
+++ js-lec-3/program-3/index.js
@@ -0,0 +1,13 @@
+// Primitive data type (string)
+let userName = "AbdulMoid"; 
+
+// Non-primitive data type (object)
+let userDetails = [
+    userName = "AbdulMoid" ,
+    age = 20 ,
+    skill = "Frontend Developer"
+];
+
+// Print values with their types
+console.log("Username:", userName, "| Type:", typeof userName);
+console.log("User Details:", userDetails, "| Type:", typeof userDetails);
