0182e3d7096343ed49844e90b4d30088351e1a41
AbdulMoid-20 <abdulmoidsiddiqui2008@gmail.com>
2025-11-04 13:34:21 +0500

    first commit

---
+++ js-lec-3/program-1/index.js
@@ -0,0 +1,15 @@
+let name = "Ali";
+let age = 30;
+let isStudent = true;
+let car = null;
+let undefined;
+let bigNumber = 1234567890123456789012345678901234567890n;
+let id = Symbol("id");
+
+console.log("value : " + name +", " + "Type : " + typeof name);
+console.log("value : " + age +", " + "Type : " + typeof age);
+console.log("value : " + isStudent +", " + "Type : " + typeof isStudent);
+console.log("value : " + car +", " + "Type : " + typeof car);
+console.log("value : " + undefined +", " + "Type : " + typeof undefined);
+console.log("value : " + bigNumber +", " + "Type : " + typeof bigNumber);
+console.log("value : " + id +", " + "Type : " + typeof id);
